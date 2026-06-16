1bf077bff4d4345661b0147a6d671db6216e3619
LSM <its10623@gmail.com>
2026-06-16 23:07:54 +0900

    feat: Hilt 사용을 위한 AndroidEntryPoint 어노테이션 추가 / 앱 아이콘 변경

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cameraapp.ui.theme.CameraAppTheme
+import com.example.cameraapp.presentation.ui.screen.CameraPreview
+import com.example.cameraapp.presentation.ui.screen.HouseHoldLedgerScreen
+import com.example.cameraapp.presentation.ui.screen.LoginScreen
+import com.example.cameraapp.presentation.ui.theme.CameraAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CameraAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HouseHoldLedgerScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CameraAppTheme {
-        Greeting("Android")
     }
 }
