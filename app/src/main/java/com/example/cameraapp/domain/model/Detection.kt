be46ae56c8c8a3db54a72c6f889658007cc9713d
LSM <its10623@gmail.com>
2026-06-16 23:05:00 +0900

    chore: 필요없는 파일 삭제

--- app/src/main/java/com/example/cameraapp/domain/model/Detection.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cameraapp.domain.model
-
-data class Detection(
-    val category: String,
-    val confidence: Float,      //신뢰성
-    val distance: Float
-)
--- app/src/main/java/com/example/cameraapp/domain/model/Guidance.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cameraapp.domain.model
-
-data class Guidance(
-    val message: String,
-    val actionHint: String,
-    val vibrationPattern: String
-)
