68160c0e91889e6b937c1608e3412fe0f516c617
LSM <its10623@gmail.com>
2026-06-16 23:13:09 +0900

    feat: 영수증 아이템 카테고리 도메인 모델 추가

---
+++ app/src/main/java/com/example/cameraapp/domain/model/Category.kt
@@ -0,0 +1,9 @@
+package com.example.cameraapp.domain.model
+
+enum class Category {
+    MEAL,        // 식비
+    GROCERY,     // 생활/마트
+    TRANSPORT,   // 교통
+    CAFE_SNACK,  // 카페/간식
+    ETC          // 기타
+}
