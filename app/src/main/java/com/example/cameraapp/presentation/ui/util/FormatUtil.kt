7b7e91bae900a8f4689e714e497b1704efc5853b
LSM <its10623@gmail.com>
2026-06-21 23:12:51 +0900

    refactor: 금액 Formatter 추가

--- app/src/main/java/com/example/cameraapp/presentation/ui/util/FormatUtil.kt
+++ app/src/main/java/com/example/cameraapp/presentation/ui/util/FormatUtil.kt
@@ -1,21 +1,56 @@
 package com.example.cameraapp.presentation.ui.util
 
 import java.text.DecimalFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 import kotlin.math.floor
 
-fun formatCalendarAmount(amount: Int, isIncome: Boolean): String {
+fun formatCalendarAmount(
+    amount: Int,
+    isIncome: Boolean,
+): String {
     val sign = if (isIncome) "+" else "-"
     return if (amount >= 10000) {
         val man = amount / 10000.0
-        val formatted = if (man == floor(man)) man.toInt().toString()
-        else String.format("%.1f", man)
+        val formatted =
+            if (man == floor(man)) {
+                man.toInt().toString()
+            } else {
+                String.format("%.1f", man)
+            }
         "${sign}${formatted}만"
     } else {
-        "${sign}${amount}"
+        "${sign}$amount"
     }
 }
 
-fun formatReceiptAmount(amount: Int): String {
+fun formatReceiptAmount(
+    amount: Int,
+    isIncome: Boolean,
+): String {
+    val sign = if (isIncome) "+" else "-"
     val formatter = DecimalFormat("#,###")
-    return formatter.format("-$amount")
+    return "${sign}${formatter.format(amount)}원"
+}
+
+fun formatItemAmount(amount: Int): String {
+    val formatter = DecimalFormat("#,###")
+    return "${formatter.format(amount)}원"
+}
+
+fun formatReceiptPurchaseAt(time: LocalDateTime): String {
+    val timeFormatter = DateTimeFormatter.ofPattern("a h:mm", Locale.KOREAN)
+    return time.format(timeFormatter)
+}
+
+fun formatTodayInfo(today: LocalDate): String {
+    val todayFormatter = DateTimeFormatter.ofPattern("M월 d일 E요일", Locale.KOREAN)
+    return today.format(todayFormatter)
+}
+
+fun formatDayOfMonth(today: LocalDateTime): String {
+    val todayFormatter = DateTimeFormatter.ofPattern("M월 d일 · a h:mm", Locale.KOREAN)
+    return today.format(todayFormatter)
 }
