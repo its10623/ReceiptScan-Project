5dbe8a40f96a3924d94c05723196ff231c7f7139
LSM <its10623@gmail.com>
2026-06-22 21:37:58 +0900

    refactor: PrimaryButton 버튼 누르는 애니메이션 추가

--- app/src/main/java/com/example/cameraapp/presentation/ui/component/PrimaryButton.kt
+++ app/src/main/java/com/example/cameraapp/presentation/ui/component/PrimaryButton.kt
@@ -1,5 +1,9 @@
 package com.example.cameraapp.presentation.ui.component
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -7,47 +11,51 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
-import com.example.cameraapp.presentation.ui.theme.CameraAppTheme
-import com.example.cameraapp.presentation.ui.theme.Primary
+import com.example.cameraapp.presentation.ui.theme.Border
 import com.example.cameraapp.presentation.ui.theme.Surface
 
 @Composable
 fun PrimaryButton(
     onClick: () -> Unit = {},
     enabled: Boolean = true,
-    modifier: Modifier = Modifier,
-    text: String? = null
+    modifier: Modifier,
+    text: String,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(if (isPressed) 0.95f else 1f)
+
     Button(
         onClick = onClick,
         enabled = enabled,
-        modifier = modifier,
-        shape = RoundedCornerShape(12.dp),
-        elevation = ButtonDefaults.buttonElevation(
-            defaultElevation = 8.dp,
-            pressedElevation = 8.dp
-        ),
-        colors = ButtonDefaults.buttonColors(
-            contentColor = Surface
-        )
+        modifier =
+            modifier
+                .scale(scale),
+        interactionSource = interactionSource,
+        shape = RoundedCornerShape(16.dp),
+        elevation =
+            ButtonDefaults.buttonElevation(
+                defaultElevation = 8.dp,
+                pressedElevation = 8.dp,
+            ),
+        colors =
+            ButtonDefaults.buttonColors(
+                contentColor = Surface,
+            ),
+        border = BorderStroke(1.dp, Border),
     ) {
         Text(
-            text = "로그인",
-            modifier = Modifier
-                .padding(8.dp),
-            style = MaterialTheme.typography.headlineMedium
-
+            text = text,
+            modifier =
+                Modifier
+                    .padding(8.dp),
+            style = MaterialTheme.typography.headlineMedium,
         )
-    }
-}
-
-@Composable
-@Preview
-fun PreviewScreen() {
-    CameraAppTheme {
-        PrimaryButton()
     }
 }
