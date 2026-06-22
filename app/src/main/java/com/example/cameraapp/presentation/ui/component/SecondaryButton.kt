ac01f60177f6b98cdedded439b2f27222ceed3ac
LSM <its10623@gmail.com>
2026-06-22 21:37:17 +0900

    feat: 자주쓰는 서브 버튼 컴포넌트 구현

---
+++ app/src/main/java/com/example/cameraapp/presentation/ui/component/SecondaryButton.kt
@@ -0,0 +1,65 @@
+package com.example.cameraapp.presentation.ui.component
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cameraapp.presentation.ui.theme.BgApp
+import com.example.cameraapp.presentation.ui.theme.Border
+import com.example.cameraapp.presentation.ui.theme.TextSub
+
+@Composable
+fun SecondaryButton(
+    onClick: () -> Unit = {},
+    modifier: Modifier,
+    text: String,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(if (isPressed) 0.95f else 1f)
+
+    Button(
+        onClick = onClick,
+        modifier =
+            modifier
+                .scale(scale),
+        interactionSource = interactionSource,
+        shape = RoundedCornerShape(16.dp),
+        elevation =
+            ButtonDefaults.buttonElevation(
+                defaultElevation = 8.dp,
+                pressedElevation = 8.dp,
+            ),
+        colors =
+            ButtonDefaults.buttonColors(
+                contentColor = TextSub,
+                containerColor = BgApp,
+            ),
+        border = BorderStroke(1.dp, Border),
+    ) {
+        Text(
+            text = text,
+            modifier =
+                Modifier
+                    .padding(8.dp),
+            style = MaterialTheme.typography.headlineMedium,
+            fontSize = 21.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
