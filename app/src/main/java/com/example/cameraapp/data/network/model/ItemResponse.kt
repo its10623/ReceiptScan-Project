3c94aaa4302e36c72b9981dc428d596d6f9f69c5
LSM <its10623@gmail.com>
2026-07-06 22:40:33 +0900

    feat: Item 응답 도메인 객체 정의

---
+++ app/src/main/java/com/example/cameraapp/data/network/model/ItemResponse.kt
@@ -0,0 +1,8 @@
+package com.example.cameraapp.data.network.model
+
+data class ItemResponse(
+    val id: Long? = null,
+    val name: String? = null,
+    val price: Int? = 0,
+    val quantity: Int = 1,
+)
