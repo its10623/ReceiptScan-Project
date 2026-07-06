cf756f2d120d14f93559b89ac8f40898cba7127f
LSM <its10623@gmail.com>
2026-07-06 22:40:06 +0900

    feat: BaseViewModel 정의

---
+++ app/src/main/java/com/example/cameraapp/presentation/base/BaseViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.cameraapp.presentation.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<Event : UiEvent, State : UiState, Effect : UiEffect> : ViewModel() {
+    abstract fun createInitialState(): State
+
+    val currentState: State
+        get() = uiState.value
+
+    private val _uiState : MutableStateFlow<State> = MutableStateFlow(createInitialState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _uiEvent : MutableSharedFlow<Event> = MutableSharedFlow()
+    val uiEvent = _uiEvent.asSharedFlow()
+
+    private val _uiEffect : Channel<Effect> = Channel()
+    val uiEffect = _uiEffect.receiveAsFlow()
+
+    init {
+        subscribeEvents()
+    }
+
+    fun setEvent(event: Event) {
+        viewModelScope.launch { _uiEvent.emit(event) }
+    }
+
+    protected fun setState(reduce: State.() -> State) {
+        _uiState.value = currentState.reduce()
+    }
+
+    protected  fun setEffect(builder: () -> Effect) {
+        viewModelScope.launch { _uiEffect.send(builder()) }
+    }
+
+    private fun subscribeEvents() {
+        viewModelScope.launch {
+            uiEvent.collect {
+                handleEvent(it)
+            }
+        }
+    }
+
+    abstract fun handleEvent(event: Event)
+}
