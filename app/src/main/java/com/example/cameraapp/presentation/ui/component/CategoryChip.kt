9029fa82e53919cab9b920c79f47c1975f06f872
LSM <its10623@gmail.com>
2026-07-09 23:47:09 +0900

    feat: Category 수정 커스텀FilterChip 구현

---
+++ app/src/main/java/com/example/cameraapp/presentation/ui/component/CategoryChip.kt
@@ -0,0 +1,52 @@
+package com.example.cameraapp.presentation.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.cameraapp.presentation.ui.theme.BgApp
+import com.example.cameraapp.presentation.ui.theme.Border
+import com.example.cameraapp.presentation.ui.theme.Shape
+import com.example.cameraapp.presentation.ui.theme.Surface
+import com.example.cameraapp.presentation.ui.theme.TextSub
+
+@Composable
+fun CategoryChip(
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color,
+) {
+    Box(
+        modifier = modifier
+            .clip(Shape.Chip)
+            .background(if (isSelected) color else Surface)
+            .border(1.dp, if (isSelected) color else TextSub.copy(alpha = 0.3f), Shape.Chip)
+            .clickable (
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { onClick() }
+            .padding(horizontal = 12.dp, vertical = 8.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = if (isSelected) FontWeight.SemiBold else FontWeight.Normal,
+            color = if (isSelected) Surface else TextSub,
+        )
+    }
+}
