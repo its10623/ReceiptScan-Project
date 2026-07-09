02022a90789d654a77633cde0a79c3867fdacab5
LSM <its10623@gmail.com>
2026-07-09 23:43:08 +0900

    feat: Loading 완료 후 & 스캔 결과 페이지 로고 컴포넌트 정의

---
+++ app/src/main/java/com/example/cameraapp/presentation/ui/component/CheckPrimaryIcon.kt
@@ -0,0 +1,45 @@
+package com.example.cameraapp.presentation.ui.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import com.example.cameraapp.presentation.ui.theme.BgApp
+import com.example.cameraapp.presentation.ui.theme.Primary
+import com.example.cameraapp.presentation.ui.theme.Shape
+
+@Composable
+fun CheckPrimaryIcon(
+    modifier: Modifier,
+    iconModifier: Modifier,
+    status: Boolean = false,
+) {
+    AnimatedVisibility(
+        visible = status,
+        enter = fadeIn(),
+        exit = fadeOut(),
+    ) {
+        Box(
+            modifier =
+                modifier
+                    .clip(Shape.Chip)
+                    .background(Primary),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Check,
+                contentDescription = "완료",
+                tint = BgApp,
+                modifier = iconModifier,
+            )
+        }
+    }
+}
