496ec1c5820215771e433828b9987483b424c9af
LSM <its10623@gmail.com>
2026-07-09 23:51:35 +0900

    feat: UserRepository 인터페이스 및 구현체 정의

---
+++ app/src/main/java/com/example/cameraapp/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.example.cameraapp.data.repository
+
+import com.example.cameraapp.domain.repository.UserRepository
+
+class UserRepositoryImpl : UserRepository
---
+++ app/src/main/java/com/example/cameraapp/domain/repository/UserRepository.kt
@@ -0,0 +1,3 @@
+package com.example.cameraapp.domain.repository
+
+interface UserRepository
