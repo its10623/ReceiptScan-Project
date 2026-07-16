9c7b713ae38c62e1d097afe3a3bff4c6d21e8bc8
its10623 <its10623@gmail.com>
2026-07-16 23:51:20 +0900

    refactor: 커스텀 네비바 아이템에서 NavigationBarItem으로 변경

--- app/src/main/java/com/example/cameraapp/presentation/ui/component/BottomNavigationBar.kt
+++ app/src/main/java/com/example/cameraapp/presentation/ui/component/BottomNavigationBar.kt
@@ -1,76 +1,45 @@
 package com.example.cameraapp.presentation.ui.component
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AccountBalanceWallet
-import androidx.compose.material.icons.rounded.BarChart
-import androidx.compose.material.icons.rounded.CalendarMonth
-import androidx.compose.material.icons.rounded.MoreHoriz
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import com.example.cameraapp.presentation.ui.screen.Screen
 import com.example.cameraapp.presentation.ui.theme.Surface
 
-@Preview
 @Composable
 fun BottomNavigationBar(
-
+    currentDestination: NavDestination?,
+    onNavigate: (Any) -> Unit
 ) {
-    Surface(
+    NavigationBar(
         modifier = Modifier
             .windowInsetsPadding(WindowInsets.navigationBars),
-        color = Surface
+        containerColor = Surface,
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(64.dp)
-                .padding(horizontal = 12.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row(modifier = Modifier.weight(1f)) {
-                NavIcon(
-                    text = "가계부",
-                    icon = Icons.Rounded.CalendarMonth,
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                )
-                NavIcon(
-                    text = "통계",
-                    icon = Icons.Rounded.BarChart,
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                )
-            }
-
-            Spacer(modifier = Modifier.width(60.dp))
-
-            Row(modifier = Modifier.weight(1f)) {
-                NavIcon(
-                    text = "자산",
-                    icon = Icons.Rounded.AccountBalanceWallet,
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                )
-                NavIcon(
-                    text = "더보기",
-                    icon = Icons.Rounded.MoreHoriz,
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                )
+        Screen.bottomNavScreen.forEachIndexed { index, bottom ->
+            NavigationBarItem(
+                selected = currentDestination?.hierarchy?.any {
+                    it.hasRoute(bottom.route::class)
+                } ?: false,
+                icon = { Icon(bottom.icon, bottom.title) },
+                label = { Text(bottom.title) },
+                onClick = { onNavigate(bottom.route) },
+            )
+            if (index == 0) {
+                Spacer(modifier = Modifier.width(60.dp))
             }
         }
     }
