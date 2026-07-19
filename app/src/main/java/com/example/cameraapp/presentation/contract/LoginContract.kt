2a0a0e03b80d9855ded1b616cb08191a4d487d88
its10623 <its10623@gmail.com>
2026-07-19 23:45:16 +0900

    feat: LoginContract 정의

---
+++ app/src/main/java/com/example/cameraapp/presentation/contract/LoginContract.kt
@@ -0,0 +1,30 @@
+package com.example.cameraapp.presentation.contract
+
+import com.example.cameraapp.presentation.base.UiIntent
+import com.example.cameraapp.presentation.base.UiSideEffect
+import com.example.cameraapp.presentation.base.UiState
+
+object LoginContract {
+    data class State(
+        val email: String = "",
+        val password: String = "",
+        val isPasswordVisible: Boolean = false,
+        val isLoading: Boolean = false,
+        val emailError: String? = null,
+        val passwordError: String? = null
+    ) : UiState
+
+    sealed class Intent : UiIntent {
+        data class EmailChanged(val email: String) : Intent()
+        data class PasswordChanged(val password: String) : Intent()
+        data object TogglePasswordVisible : Intent()
+        data object Login : Intent()
+        // data class SocialLogin() : Intent()
+    }
+
+    sealed class SideEffect : UiSideEffect {
+        data class NavigateToMain(val userKey: String) : SideEffect()
+
+        data class ShowError(val message: String) : SideEffect()
+    }
+}
