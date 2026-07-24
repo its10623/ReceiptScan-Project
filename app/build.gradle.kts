0cb22eec87a4cee1bc8b9f8aa27a9622ac92ebc0
its10623 <its10623@gmail.com>
2026-07-24 11:01:16 +0900

    refactor: 패키지명 example에서 smoothsm 으로 변경(추가)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.cameraapp"
+        applicationId = "com.smoothsm.cameraapp"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
